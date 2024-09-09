905e6a71481b33829f4df91fff4b58406adc488d
yVentura7 <168231053+yVentura7@users.noreply.github.com>
2024-09-09 18:29:44 -0500

    Initial commit

---
+++ app/src/main/java/com/example/cupcake/CupcakeScreen.kt
@@ -0,0 +1,167 @@
+/*
+ * Copyright (C) 2023 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.example.cupcake
+
+import androidx.compose.foundation.layout.fillMaxHeight
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.ArrowBack
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
+import androidx.compose.material3.TopAppBarDefaults
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.getValue
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.res.dimensionResource
+import androidx.compose.ui.res.stringResource
+import androidx.lifecycle.viewmodel.compose.viewModel
+import androidx.navigation.NavHostController
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
+import com.example.cupcake.data.DataSource
+import com.example.cupcake.ui.OrderSummaryScreen
+import com.example.cupcake.ui.OrderViewModel
+import com.example.cupcake.ui.SelectOptionScreen
+import com.example.cupcake.ui.StartOrderScreen
+
+
+enum class CupcakeScreen() {
+    Start,
+    Flavor,
+    Pickup,
+    Summary
+}
+
+@Composable
+fun CupcakeAppBar(
+    canNavigateBack: Boolean,
+    navigateUp: () -> Unit,
+    modifier: Modifier = Modifier
+) {
+    TopAppBar(
+        title = { Text(stringResource(id = R.string.app_name)) },
+        colors = TopAppBarDefaults.mediumTopAppBarColors(
+            containerColor = MaterialTheme.colorScheme.primaryContainer
+        ),
+        modifier = modifier,
+        navigationIcon = {
+            if (canNavigateBack) {
+                IconButton(onClick = navigateUp) {
+                    Icon(
+                        imageVector = Icons.Filled.ArrowBack,
+                        contentDescription = stringResource(R.string.back_button)
+                    )
+                }
+            }
+        }
+    )
+}
+
+@Composable
+fun CupcakeApp(
+    viewModel: OrderViewModel = viewModel(),
+    navController: NavHostController = rememberNavController()
+) {
+
+    Scaffold(
+
+        topBar = {
+            CupcakeAppBar(
+                canNavigateBack = false,
+                navigateUp = { /* TODO: implement back navigation */ }
+            )
+        }
+    ) { innerPadding ->
+        val uiState by viewModel.uiState.collectAsState()
+        NavHost(
+            navController = navController,
+            startDestination = CupcakeScreen.Start.name,
+            modifier = Modifier.padding(innerPadding)
+        ) {
+            composable(route = CupcakeScreen.Start.name) {
+                StartOrderScreen(
+                    quantityOptions = DataSource.quantityOptions,
+                    onNextButtonClicked = {
+                        viewModel.setQuantity(it) // Actualizamos la cantidad en el ViewModel
+                        navController.navigate(CupcakeScreen.Flavor.name) // Navegamos a la pantalla de selección de sabor
+                    },
+                    modifier = Modifier
+                        .fillMaxSize()
+                        .padding(dimensionResource(R.dimen.padding_medium))
+                )
+            }
+            composable(route = CupcakeScreen.Flavor.name) {
+                val context = LocalContext.current
+                SelectOptionScreen(
+                    subtotal = uiState.price,
+                    onNextButtonClicked = {
+                        navController.navigate(CupcakeScreen.Pickup.name) // Navegamos a la pantalla de selección de fecha
+                    },
+                    onCancelButtonClicked = {
+                        cancelOrderAndNavigateToStart(viewModel, navController) // Cancelamos el pedido y volvemos a la pantalla de inicio
+                    },
+                    options = DataSource.flavors.map { id -> context.resources.getString(id) },
+                    onSelectionChanged = { viewModel.setFlavor(it) },
+                    modifier = Modifier.fillMaxHeight()
+                )
+            }
+            composable(route = CupcakeScreen.Pickup.name) {
+                SelectOptionScreen(
+                    subtotal = uiState.price,
+                    onNextButtonClicked = {
+                        navController.navigate(CupcakeScreen.Summary.name) // Navegamos a la pantalla de resumen
+                    },
+                    onCancelButtonClicked = {
+                        cancelOrderAndNavigateToStart(viewModel, navController) // Cancelamos el pedido y volvemos a la pantalla de inicio
+                    },
+                    options = uiState.pickupOptions,
+                    onSelectionChanged = { viewModel.setDate(it) },
+                    modifier = Modifier.fillMaxHeight()
+                )
+            }
+            composable(route = CupcakeScreen.Summary.name) {
+                OrderSummaryScreen(
+                    orderUiState = uiState,
+                    onCancelButtonClicked = {
+                        cancelOrderAndNavigateToStart(viewModel, navController) // Cancelamos el pedido y volvemos a la pantalla de inicio
+                    },
+                    onSendButtonClicked = { subject: String, summary: String ->
+                        // Implementación futura para enviar el resumen del pedido
+                    },
+                    modifier = Modifier.fillMaxHeight()
+                )
+            }
+        }
+
+    }
+}
+
+// Función para cancelar el pedido y navegar de regreso a la pantalla de inicio
+private fun cancelOrderAndNavigateToStart(
+    viewModel: OrderViewModel,  // ViewModel para manejar el estado de la orden
+    navController: NavHostController // Controlador de navegación para gestionar la pila de actividades
+) {
+    viewModel.resetOrder() // Resetea el estado de la orden en el ViewModel
+    navController.popBackStack(CupcakeScreen.Start.name, inclusive = false) // Navega de regreso a la pantalla de inicio
+}
---
+++ app/src/main/java/com/example/cupcake/ui/SummaryScreen.kt
@@ -0,0 +1,150 @@
+/*
+ * Copyright (C) 2023 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.example.cupcake.ui
+
+import android.content.Intent
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxHeight
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Button
+import androidx.compose.material3.Divider
+import androidx.compose.material3.OutlinedButton
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.res.dimensionResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.tooling.preview.Preview
+import com.example.cupcake.R
+import com.example.cupcake.data.OrderUiState
+import com.example.cupcake.ui.components.FormattedPriceLabel
+import com.example.cupcake.ui.theme.CupcakeTheme
+
+/**
+ * This composable expects [orderUiState] that represents the order state, [onCancelButtonClicked]
+ * lambda that triggers canceling the order and passes the final order to [onSendButtonClicked]
+ * lambda
+ */
+@Composable
+fun OrderSummaryScreen(
+    orderUiState: OrderUiState,
+    onCancelButtonClicked: () -> Unit,  // Agregar este parámetro
+    onSendButtonClicked: (String, String) -> Unit,  // Agregar este parámetro
+    modifier: Modifier = Modifier
+) {
+    val context = LocalContext.current
+
+    val resources = LocalContext.current.resources
+    val numberOfCupcakes = resources.getQuantityString(
+        R.plurals.cupcakes,
+        orderUiState.quantity,
+        orderUiState.quantity
+    )
+
+    // Carga y formatea un string con los detalles del pedido
+    val orderSummary = stringResource(
+        R.string.order_details,
+        numberOfCupcakes,
+        orderUiState.flavor,
+        orderUiState.date,
+        orderUiState.quantity
+    )
+
+    val newOrder = stringResource(R.string.new_cupcake_order)
+
+    // Crear un intent para compartir el pedido
+    val shareIntent = Intent(Intent.ACTION_SEND).apply {
+        type = "text/plain"
+        putExtra(Intent.EXTRA_SUBJECT, newOrder)
+        putExtra(Intent.EXTRA_TEXT, orderSummary)
+    }
+
+    // Lista de items para mostrar el resumen del pedido
+    val items = listOf(
+        Pair(stringResource(R.string.quantity), numberOfCupcakes),
+        Pair(stringResource(R.string.flavor), orderUiState.flavor),
+        Pair(stringResource(R.string.pickup_date), orderUiState.date)
+    )
+
+    Column(
+        modifier = modifier,
+        verticalArrangement = Arrangement.SpaceBetween
+    ) {
+        Column(
+            modifier = Modifier.padding(dimensionResource(R.dimen.padding_medium)),
+            verticalArrangement = Arrangement.spacedBy(dimensionResource(R.dimen.padding_small))
+        ) {
+            items.forEach { item ->
+                Text(item.first.uppercase())
+                Text(text = item.second, fontWeight = FontWeight.Bold)
+                Divider(thickness = dimensionResource(R.dimen.thickness_divider))
+            }
+            Spacer(modifier = Modifier.height(dimensionResource(R.dimen.padding_small)))
+            FormattedPriceLabel(
+                subtotal = orderUiState.price,
+                modifier = Modifier.align(Alignment.End)
+            )
+        }
+        Row(
+            modifier = Modifier.padding(dimensionResource(R.dimen.padding_medium))
+        ) {
+            Column(
+                verticalArrangement = Arrangement.spacedBy(dimensionResource(R.dimen.padding_small))
+            ) {
+                Button(
+                    modifier = Modifier.fillMaxWidth(),
+                    onClick = {
+                        // Lanza la ShareSheet
+                        context.startActivity(
+                            Intent.createChooser(shareIntent, null)
+                        )
+                        // Llama a la función para procesar el envío si es necesario
+                        onSendButtonClicked(newOrder, orderSummary)
+                    }
+                ) {
+                    Text(stringResource(R.string.send))
+                }
+                OutlinedButton(
+                    modifier = Modifier.fillMaxWidth(),
+                    onClick = onCancelButtonClicked
+                ) {
+                    Text(stringResource(R.string.cancel))
+                }
+            }
+        }
+    }
+}
+
+@Preview
+@Composable
+fun OrderSummaryPreview() {
+    CupcakeTheme {
+        OrderSummaryScreen(
+            orderUiState = OrderUiState(0, "Test", "Test", "$300.00"),
+            onCancelButtonClicked = {},
+            onSendButtonClicked = { _, _ -> },
+            modifier = Modifier.fillMaxHeight()
+        )
+    }
+}
